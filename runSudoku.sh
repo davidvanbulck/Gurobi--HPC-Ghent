b3f121037cc22d14b5d9abd61706dc13e49d141b
vsc41980 vscuser <vsc41980@gligar05.gastly.os>
2019-12-18 16:07:12 +0100

    Initialized repository with documentation for running gurobi on github

---
+++ runSudoku.sh
@@ -0,0 +1,28 @@
+# Job script for a minimal working example to solve a sudoku
+
+# Define job specifications
+
+# 1 nodes, 1 processor (note: processor = core)
+# Note: do not forget to limit number of threads Gurobi can use (otherwise Gurobi thinks it has all processors available on the cluster whereas it has only one available)
+#PBS -l  nodes=1:ppn=1
+
+# Expected running time 5 seconds
+#PBS -l walltime=0:00:05
+
+# Use infiniband nodes (important when you use mulitple cores)
+#PBS -l ib
+
+# Move to the working directory
+# Alternatively: $ cd  /user/gent/419/vsc41980/gurobi/sudoku_code
+cd ~/gurobi/
+
+# Load modules
+module load Gurobi/9.0.0
+
+# Compile the program
+make sudoku
+
+# Solve instance
+# "<" makes that de data of sudoku1.dat is correctly passed to Gurobi
+./sudoku < ~/gurobi/sudoku1.dat
+
